97894c7eaee24c4a487394d48856ca0385e927e3
JonaszJestem <jonaszwiacek@gmail.com>
2021-08-03 13:18:24 +0200

    fix: Directory name

--- src/core/image/image.controller.ts
+++ src/core/image/image.controller.ts
@@ -1,8 +1,8 @@
 import type { FastifyInstance } from 'fastify';
-import { createResizeHandler } from './resize/resize.handler';
-import { createMetadataHandler } from './resize/metadata.handler';
-import { createCropHandler } from './resize/crop.handler';
-import { createThumbnailHandler } from './resize/thumbnail.handler';
+import { createResizeHandler } from './handlers/resize.handler';
+import { createMetadataHandler } from './handlers/metadata.handler';
+import { createCropHandler } from './handlers/crop.handler';
+import { createThumbnailHandler } from './handlers/thumbnail.handler';
 
 export const imageController = async (fastify: FastifyInstance) => {
   createMetadataHandler('/metadata', fastify);
