003c9a3bf47fc1ce47d00f6e4f9ace85d003b2ab
JonaszJestem <jonaszwiacek@gmail.com>
2021-08-09 10:03:21 +0200

    fix: CR Comments

--- src/core/image/image.service.test.ts
+++ src/core/image/image.service.test.ts
@@ -1,10 +1,14 @@
 import { ImageService } from './image.service';
-import { getTestImage, getTestImageBuffer, streamToBuffer } from '../../../test/test.utils';
+import {
+  getImageDimensions,
+  getTestImage,
+  getTestImageBuffer,
+  streamToBuffer,
+} from '../../../test/test.utils';
 import type { Region } from 'sharp';
 import sharp from 'sharp';
 
 import getColors from 'get-image-colors';
-
 
 describe('ImageService', () => {
   let imageService: ImageService;
@@ -17,9 +21,11 @@ describe('ImageService', () => {
       const expectedDimensions = 20;
       const image = await getTestImage();
 
-      const resizedImage = imageService.resize(image, {height: expectedDimensions});
+      const resizedImage = imageService.resize(image, {
+        height: expectedDimensions,
+      });
 
-      const resizedImageMetadata = await sharp(await streamToBuffer(resizedImage)).metadata();
+      const resizedImageMetadata = await getImageDimensions(resizedImage);
       expect(resizedImageMetadata).toMatchObject({
         width: expectedDimensions,
         height: expectedDimensions,
@@ -30,12 +36,17 @@ describe('ImageService', () => {
   describe('#crop', () => {
     it('should crop passed image', async () => {
       const expectedDimensions = 1;
-      const cropOptions = {top: expectedDimensions, height: expectedDimensions, width: expectedDimensions, left: expectedDimensions} as Region;
+      const cropOptions = {
+        top: expectedDimensions,
+        height: expectedDimensions,
+        width: expectedDimensions,
+        left: expectedDimensions,
+      } as Region;
       const image = await getTestImage();
 
       const croppedImage = imageService.crop(image, cropOptions);
 
-      const croppedImageMetadata = await sharp(await streamToBuffer(croppedImage)).metadata();
+      const croppedImageMetadata = await getImageDimensions(croppedImage);
       expect(croppedImageMetadata).toMatchObject({
         width: expectedDimensions,
         height: expectedDimensions,
@@ -45,21 +56,31 @@ describe('ImageService', () => {
     it('should crop proper region of the image', async () => {
       const expectedDimensions = 1;
       const expectedColor = '#043404';
-      const cropOptions = {top: expectedDimensions, height: expectedDimensions, width: expectedDimensions, left: expectedDimensions} as Region;
+      const cropOptions = {
+        top: expectedDimensions,
+        height: expectedDimensions,
+        width: expectedDimensions,
+        left: expectedDimensions,
+      } as Region;
       const image = await getTestImage();
 
       const croppedImage = imageService.crop(image, cropOptions);
 
-      const colors = await getColors(await streamToBuffer(croppedImage), 'image/jpeg');
-      expect(colors.length).toEqual(1);
-      expect(colors[0].hex()).toEqual(expectedColor);
+      const colors = await getColors(
+        await streamToBuffer(croppedImage),
+        'image/jpeg',
+      );
+      expect(colors).toHaveLength(1);
+      expect(colors[0].hex()).toBe(expectedColor);
     });
   });
 
   describe('#metadata', () => {
     it('should retrieve metadata of passed file', async () => {
       const image = await getTestImage();
-      const expectedMetadata = await sharp(await getTestImageBuffer()).metadata();
+      const expectedMetadata = await sharp(
+        await getTestImageBuffer(),
+      ).metadata();
 
       const metadata = await imageService.metadata(image);
 
--- test/test.utils.ts
+++ test/test.utils.ts
@@ -2,6 +2,7 @@ import type { Stream } from 'stream';
 import { Readable } from 'stream';
 import fs from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 
 export const getTestImage = async (): Promise<Readable> => {
   const image = await getTestImageBuffer();
@@ -11,8 +12,8 @@ export const getTestImage = async (): Promise<Readable> => {
 
 export const TEST_IMAGE_PATH = 'test/resources/test-image.jpg';
 
-export const getTestImageBuffer = (): Promise<Buffer> => {
-  return fs.promises.readFile(path.join(process.cwd(), TEST_IMAGE_PATH));
+export const getTestImageBuffer = async (): Promise<Buffer> => {
+  return await fs.promises.readFile(path.join(process.cwd(), TEST_IMAGE_PATH));
 };
 
 export const streamToBuffer = (stream: Stream): Promise<Buffer> => {
@@ -30,6 +31,14 @@ export const streamToBuffer = (stream: Stream): Promise<Buffer> => {
     stream.on('error', (err) => {
       reject(err);
     });
-
   });
+};
+
+export const getImageDimensions = async (resizedImage: Stream) => {
+  const dimensions = await sharp(await streamToBuffer(resizedImage)).metadata();
+
+  return {
+    height: dimensions.height,
+    width: dimensions.width,
+  };
 };
