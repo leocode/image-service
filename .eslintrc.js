1df4146b85c733c22d05933e8fcae70eb363d718
JonaszJestem <jonaszwiacek@gmail.com>
2021-08-11 08:38:31 +0200

    feat: Require `await`ing returned promises

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    extends: '@leocode/eslint-config/node',
+  extends: "@leocode/eslint-config/node",
+  rules: {
+    "no-return-await": "off",
+    "@typescript-eslint/return-await": ["error", "always"],
+  },
 };
