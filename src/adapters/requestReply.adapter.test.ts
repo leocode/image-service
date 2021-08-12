afae4f19c32ed4050208d9c2e84722b8445331f9
Ireneusz Pliś <ireneusz.plis+github@gmail.com>
2021-08-12 10:47:44 +0200

    video test added

--- src/adapters/requestReply.adapter.test.ts
+++ src/adapters/requestReply.adapter.test.ts
@@ -1,18 +1,28 @@
 import { requestReplyAdapter } from './requestReply.adapter';
 import {
-  getTestImage,
+  getTestImage, getTestVideo,
 } from '../../test/test.utils';
 import { FileTypeEnum } from './adapter.types';
 
 describe('RequestReplyAdapter', () => {
   describe('handleFile', () => {
-    it('should return file', async () => {
+    it('should return image file', async () => {
       const image = await getTestImage();
 
       const adapterResult = await requestReplyAdapter.handleFile({ file: image, fileType: FileTypeEnum.image, requestBody: {}});
 
       expect(adapterResult).toMatchObject({
         file: image,
+      });
+    });
+
+    it('should return video file', async () => {
+      const video = await getTestVideo();
+
+      const adapterResult = await requestReplyAdapter.handleFile({ file: video, fileType: FileTypeEnum.video, requestBody: {}});
+
+      expect(adapterResult).toMatchObject({
+        file: video,
       });
     });
   });
